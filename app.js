c7b022ceca254b664bfeae12fa3b9822ec5971ec
Kaptain-T <ezighi.antony@gmail.com>
2024-05-28 17:48:11 +0100

    first commit of the cloned files to repo. Error messages are almost all set

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
-
-console.log(firstName);
+const exclamationIcon = document.querySelector(".fa-circle-exclamation")
 
 // console.log(firstName, lastName, email, password);
 
@@ -16,32 +15,64 @@ form.addEventListener('submit', (e) => {
   const emailVal = email.value;
   const passwordVal = password.value;
   console.log(fName, lName, emailVal, passwordVal);
+  const showIcons = function(){
+    exclamationIcon.classList.toggle("show");
+  }
+  
 
   // Check first name
+  const firstnamemessage = `
+  <small class="error-message">First Name cannot be empty</small>
+  `;
   if (fName === '') {
     firstName.classList.add('error');
+    showIcons();
+    if (firstName.nextElementSibling == null){
+      firstName.insertAdjacentHTML("afterend", firstnamemessage);
+    }
   } else {
     firstName.classList.remove('error');
   }
-  // Check last name
 
+  // Check last name
+  const lastnamemessage = `
+  <small class="error-message">Last Name cannot be empty</small>
+  `;
   if (lName === '') {
     lastName.classList.add('error');
+    showIcons();
+    if (lastName.nextElementSibling == null){
+      lastName.insertAdjacentHTML("afterend", lastnamemessage);
+    }
   } else {
     lastName.classList.remove('error');
   }
-  // Check email
 
+  // Check email
+  const emailmessage = `
+  <small class="error-message">Looks like this is not an email</small>
+  `;
   if (!validateEmail(emailVal) || emailVal === '') {
     email.classList.add('error');
+    showIcons();
+    if (email.nextElementSibling == null){
+    email.insertAdjacentHTML("afterend", emailmessage);
+    email.placeholder = "email@example/com";
+    }
   } else {
     email.classList.remove('error');
   }
 
   // Check password
-
+  const passwordmessage = `
+  <small class="error-message">Password cannot be empty</small>
+  `;
   if (passwordVal === '') {
     password.classList.add('error');
+    showIcons();
+    if (password.nextElementSibling == null){
+      password.insertAdjacentHTML("afterend", passwordmessage);
+    }
   } else {
     password.classList.remove('error');
   }
